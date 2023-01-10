704c0cd32390a78d536ba55c790fb789f712eb68
DarkSeriusCode <stepanharkov19@gmail.com>
2023-01-10 21:55:26 +0300

    Added Lua LSP server

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -2,13 +2,13 @@ local lsp = require("lspconfig")
 local cmp_cap = require("cmp_nvim_lsp").default_capabilities()
 local navic = require("nvim-navic")
 
-function navic_on_attach(client, bufnr)
+local function navic_on_attach(client, bufnr)
     if client.server_capabilities.documentSymbolProvider then
         navic.attach(client, bufnr)
     end
 end
 
-local servers = {lsp.rust_analyzer, lsp.clangd}
+local servers = {lsp.rust_analyzer, lsp.clangd, lsp.sumneko_lua}
 
 for _, server in ipairs(servers) do
     server.setup({
