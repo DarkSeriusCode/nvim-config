b250cf84c94ca09474762997e3364b867e566bf5
DarkSeriusCode <stepanharkov19@gmail.com>
2023-07-26 19:49:59 +0300

    Configured Telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,8 @@
 require("telescope").setup({
     defaults = {
+        layout_strategy = "vertical",
+        prompt_prefix = ": ",
+        selection_caret = " ",
         file_ignore_patterns = {"target", "build", "docs"},
     },
 })
