70d60b39c628408e650793742eacbf12f6bb5638
DarkSeriusCode <stepanharkov19@gmail.com>
2024-08-10 11:21:18 +0300

    Changed an error icon at bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -20,7 +20,7 @@ local function config()
             diagnostics_indicator = function(_, _, diagnostics_dict, _)
                 local s = " "
                 for e, n in pairs(diagnostics_dict) do
-                        local sym = e == "error" and " " or (e == "warning" and " " or "" )
+                        local sym = e == "error" and " " or (e == "warning" and " " or "" )
                         s = s .. n .. sym
                 end
                 return s
